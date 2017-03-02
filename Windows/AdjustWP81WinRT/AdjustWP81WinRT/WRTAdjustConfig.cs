5205884653b8666b2ba0bb85c446bb6bc88a9e35
uerceg <ugi@adjust.com>
2017-03-02 15:17:23 +0100

    New version 4.11.1

--- Windows/AdjustWP81WinRT/AdjustWP81WinRT/WRTAdjustConfig.cs
+++ Windows/AdjustWP81WinRT/AdjustWP81WinRT/WRTAdjustConfig.cs
@@ -52,7 +52,7 @@ namespace AdjustWinRT
         {
             this.appToken = appToken;
             this.environment = environment;
-            this.sdkPrefix = "cocos2d-x4.11.0";
+            this.sdkPrefix = "cocos2d-x4.11.1";
             
             this.logLevel = LogLevel.Info;
 
