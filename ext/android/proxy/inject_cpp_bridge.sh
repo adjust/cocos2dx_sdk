08239de3b50092dca9a90629ebae1a7c52b8c8fe
serj <srdjan@adjust.com>
2018-09-24 14:25:46 +0200

    cpp bridge script output adjusted

--- ext/android/proxy/inject_cpp_bridge.sh
+++ ext/android/proxy/inject_cpp_bridge.sh
@@ -17,28 +17,28 @@ JAVAC=$JAVA_HOME_7/bin/javac
 
 # ======================================== #
 
-echo -e "${CYAN}[ADJUST][ANDROID][BUILD]:${GREEN} Unpacking adjust-android.jar file ... ${NC}"
+echo -e "${CYAN}* [ADJUST-SDK-BUILD][CPP-BRIDGE]:${GREEN} Unpacking adjust-android.jar file ... ${NC}"
 $JAVAC -cp "adjust-android.jar:$ANDROID_JAR" com/adjust/sdk/*.java
-echo -e "${CYAN}[ADJUST][ANDROID][BUILD]:${GREEN} Done! ${NC}"
+echo -e "${CYAN}* [ADJUST-SDK-BUILD][CPP-BRIDGE]:${GREEN} Done! ${NC}"
 
 # ======================================== #
 
 if [ $# -eq 1 ] && [ $1 == --with-test-lib ]; then
-    echo -e "${CYAN}[ADJUST][ANDROID][BUILD]:${GREEN} Unpacking adjust-testing.jar file ... ${NC}"
+    echo -e "${CYAN}* [ADJUST-SDK-BUILD][CPP-BRIDGE]:${GREEN} Unpacking adjust-testing.jar file ... ${NC}"
     $JAVAC -cp "adjust-testing.jar:$ANDROID_JAR" com/adjust/testlibrary/*.java
-    echo -e "${CYAN}[ADJUST][ANDROID][BUILD]:${GREEN} Done! ${NC}"
+    echo -e "${CYAN}* [ADJUST-SDK-BUILD][CPP-BRIDGE]:${GREEN} Done! ${NC}"
 fi
 
 # ======================================== #
 
-echo -e "${CYAN}[ADJUST][ANDROID][BUILD]:${GREEN} Injecting C++ bridge Java classes to adjust-android.jar ... ${NC}"
+echo -e "${CYAN}* [ADJUST-SDK-BUILD][CPP-BRIDGE]:${GREEN} Injecting C++ bridge Java classes to adjust-android.jar ... ${NC}"
 $JAR uf adjust-android.jar com/adjust/sdk/*.class
-echo -e "${CYAN}[ADJUST][ANDROID][BUILD]:${GREEN} Done! ${NC}"
+echo -e "${CYAN}* [ADJUST-SDK-BUILD][CPP-BRIDGE]:${GREEN} Done! ${NC}"
 
 # ======================================== #
 
 if [ $# -eq 1 ] && [ $1 == --with-test-lib ]; then
-    echo -e "${CYAN}[ADJUST][ANDROID][BUILD]:${GREEN} Injecting C++ bridge Java classes to adjust-testing.jar ... ${NC}"
+    echo -e "${CYAN}* [ADJUST-SDK-BUILD][CPP-BRIDGE]:${GREEN} Injecting C++ bridge Java classes to adjust-testing.jar ... ${NC}"
     $JAR uf adjust-testing.jar com/adjust/testlibrary/*.class
-    echo -e "${CYAN}[ADJUST][ANDROID][BUILD]:${GREEN} Done! ${NC}"
+    echo -e "${CYAN}* [ADJUST-SDK-BUILD][CPP-BRIDGE]:${GREEN} Done! ${NC}"
 fi
