6fb66a2c8c410c3ab337a10314b623f5563a0023
uerceg <ugi@adjust.com>
2024-09-26 13:32:38 +0200

    build: switch to static xcframework

--- scripts/build_sdk_ios.py
+++ scripts/build_sdk_ios.py
@@ -8,15 +8,15 @@ def build(root_dir, ios_submodule_dir, apptype, configuration, app_path):
 
 def build_example(root_dir, ios_submodule_dir, configuration, app_path):
     # ------------------------------------------------------------------
-    # Building AdjustSdk.framework file.
-    debug_green('Building AdjustSdk.framework file ...')
+    # Building AdjustSdk.xcframework file.
+    debug_green('Building AdjustSdk.xcframework file ...')
     _build_sdk(root_dir, ios_submodule_dir, configuration, with_test_lib=False)
 
     # ------------------------------------------------------------------
-    # Copying new AdjustSdk.framework to example app.
-    debug_green('Copying new AdjustSdk.framework to example app ...')
-    remove_dir_if_exists('{0}/proj.ios_mac/ios/AdjustSdk.framework'.format(app_path))
-    copy_dir_contents('{0}/libs/ios/AdjustSdk.framework'.format(root_dir), '{0}/proj.ios_mac/ios/AdjustSdk.framework'.format(app_path), copy_symlinks=True)
+    # Copying new AdjustSdk.xcframework to example app.
+    debug_green('Copying new AdjustSdk.xcframework to example app ...')
+    remove_dir_if_exists('{0}/proj.ios_mac/ios/AdjustSdk.xcframework'.format(app_path))
+    copy_dir_contents('{0}/libs/ios/AdjustSdk.xcframework'.format(root_dir), '{0}/proj.ios_mac/ios/AdjustSdk.xcframework'.format(app_path), copy_symlinks=True)
 
     # ------------------------------------------------------------------
     # Updating Adjust SDK C++ source files in ${APP_PATH}/Classes/Adjust fodler.
@@ -33,16 +33,16 @@ def build_example(root_dir, ios_submodule_dir, configuration, app_path):
 
 def build_test(root_dir, ios_submodule_dir, configuration, app_path):
     # ------------------------------------------------------------------
-    # Building AdjustSdk.framework and AdjustTestLibrary.framework file.
-    debug_green('Building AdjustSdk.framework and AdjustTestLibrary.framework file ...')
+    # Building AdjustSdk.xcframework and AdjustTestLibrary.framework file.
+    debug_green('Building AdjustSdk.xcframework and AdjustTestLibrary.framework file ...')
     _build_sdk(root_dir, ios_submodule_dir, configuration, with_test_lib=True)
 
     # ------------------------------------------------------------------
-    # Copying new AdjustSdk.framework & AdjustTestLibrary.framework to example app.
-    debug_green('Copying new AdjustSdk.framework & AdjustTestLibrary.framework to test app ...')
-    remove_dir_if_exists('{0}/proj.ios_mac/ios/AdjustSdk.framework'.format(app_path))
+    # Copying new AdjustSdk.xcframework & AdjustTestLibrary.framework to example app.
+    debug_green('Copying new AdjustSdk.xcframework & AdjustTestLibrary.framework to test app ...')
+    remove_dir_if_exists('{0}/proj.ios_mac/ios/AdjustSdk.xcframework'.format(app_path))
     remove_dir_if_exists('{0}/proj.ios_mac/ios/AdjustTestLibrary.framework'.format(app_path))
-    copy_dir_contents('{0}/libs/ios/AdjustSdk.framework'.format(root_dir), '{0}/proj.ios_mac/ios/AdjustSdk.framework'.format(app_path), copy_symlinks=True)
+    copy_dir_contents('{0}/libs/ios/AdjustSdk.xcframework'.format(root_dir), '{0}/proj.ios_mac/ios/AdjustSdk.xcframework'.format(app_path), copy_symlinks=True)
     copy_dir_contents('{0}/test/libs/ios/AdjustTestLibrary.framework'.format(root_dir), '{0}/proj.ios_mac/ios/AdjustTestLibrary.framework'.format(app_path), copy_symlinks=True)
 
     # ------------------------------------------------------------------
@@ -56,7 +56,7 @@ def build_test(root_dir, ios_submodule_dir, configuration, app_path):
     # Populate dist folder with files needed for clients.
     update_dist(root_dir)
 
-    debug_green('Success. Make sure AdjustSdk.framework & AdjustTestLibrary.framework is linked in "Build Phases". Run it with Xcode from "proj.ios_mac" directory.')
+    debug_green('Success. Make sure AdjustSdk.xcframework & AdjustTestLibrary.framework is linked in "Build Phases". Run it with Xcode from "proj.ios_mac" directory.')
 
 def _build_sdk(root_dir, ios_submodule_dir, configuration, with_test_lib=False):
     # ------------------------------------------------------------------
@@ -67,9 +67,9 @@ def _build_sdk(root_dir, ios_submodule_dir, configuration, with_test_lib=False):
     test_lib_out_dir   = '{0}/test/libs/ios'.format(root_dir)
 
     # ------------------------------------------------------------------
-    # Deleting old AdjustSdk.framework file.
-    debug_green('Deleting old AdjustSdk.framework file ...')
-    remove_dir_if_exists('{0}/AdjustSdk.framework'.format(lib_out_dir))
+    # Deleting old AdjustSdk.xcframework file.
+    debug_green('Deleting old AdjustSdk.xcframework file ...')
+    remove_dir_if_exists('{0}/AdjustSdk.xcframework'.format(lib_out_dir))
 
     # ------------------------------------------------------------------
     # Deleting old AdjustTestLibrary.framework file.
@@ -78,19 +78,19 @@ def _build_sdk(root_dir, ios_submodule_dir, configuration, with_test_lib=False):
         remove_dir_if_exists('{0}/AdjustTestLibrary.framework'.format(test_lib_out_dir))
 
     # ------------------------------------------------------------------
-    # Rebuilding AdjustSdk.framework file.
-    debug_green('Rebuilding AdjustSdk.framework file ...')
+    # Rebuilding AdjustSdk.xcframework file.
+    debug_green('Rebuilding AdjustSdk.xcframework file ...')
     change_dir(build_dir)
     # if configuration == 'debug':
     #     xcode_build('AdjustStatic', 'Debug')
     # else:
     #     xcode_build('AdjustStatic', 'Release')
-    execute_command(['./scripts/build_frameworks.sh', '-fs', '-ios', '-tv'])
+    execute_command(['./scripts/build_frameworks.sh', '-xs', '-ios', '-tv'])
 
     # ------------------------------------------------------------------
-    # Copying AdjustSdk.framework to ${ROOT_DIR}/${LIB_OUT_DIR}.
-    debug_green('Copying AdjustSdk.framework to {0} ...'.format(lib_out_dir))
-    copy_dir_contents('{0}/sdk_distribution/frameworks-static/AdjustSdk-iOS-Static/AdjustSdk.framework'.format(build_dir), '{0}/AdjustSdk.framework'.format(lib_out_dir), copy_symlinks=True)
+    # Copying AdjustSdk.xcframework to ${ROOT_DIR}/${LIB_OUT_DIR}.
+    debug_green('Copying AdjustSdk.xcframework to {0} ...'.format(lib_out_dir))
+    copy_dir_contents('{0}/sdk_distribution/xcframeworks-static/AdjustSdk-iOS-tvOS-xcframework/AdjustSdk.xcframework'.format(build_dir), '{0}/AdjustSdk.xcframework'.format(lib_out_dir), copy_symlinks=True)
 
     # ------------------------------------------------------------------
     # Rebuilding AdjustTestLibrary.framework file.
@@ -101,7 +101,7 @@ def _build_sdk(root_dir, ios_submodule_dir, configuration, with_test_lib=False):
         #     xcode_build('AdjustTestLibraryStatic', 'Debug')
         # else:
         #     xcode_build('AdjustTestLibraryStatic', 'Release')
-        execute_command(['./scripts/build_frameworks.sh', '-test', '-ios', '-tv'])
+        execute_command(['./scripts/build_frameworks.sh', '-test', '-ios'])
 
         # ------------------------------------------------------------------
         # Copying AdjustTestLibrary.framework to ${ROOT_DIR}/${LIB_OUT_DIR}.
